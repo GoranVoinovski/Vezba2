0a9a449da1cb62819deb60d7db75b944bddf3c15
Goran <gogovoinovski@gmail.com>
2018-01-05 15:38:06 +0100

    Pocetno vtora faza

--- app/src/main/java/com/example/goran/vezba/MainActivity.java
+++ app/src/main/java/com/example/goran/vezba/MainActivity.java
@@ -1,16 +1,65 @@
 package com.example.goran.vezba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+
+import com.example.goran.vezba.Adapteri.RVRestoraniAdapter;
+import com.example.goran.vezba.Listeners.RestoraniOnClickListener;
+import com.example.goran.vezba.Modeli.Restorani;
+import com.example.goran.vezba.Modeli.RestoraniModel;
+import com.example.goran.vezba.PreferencesModeli.RestoraniPreferences;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv)RecyclerView moeRView;
+    RestoraniModel model = new RestoraniModel();
+    RVRestoraniAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        
+        adapter = new RVRestoraniAdapter(this, new RestoraniOnClickListener() {
+            @Override
+            public void
+            onRestoranClick(Restorani restoran, int position) {
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("pozicija", position);
+                startActivityForResult(intent, 1000);}
+
+            @Override
+            public void onRestoranLongClick(Restorani restoran, final int position) {
+
+                        model.restaurants.remove(position);
+                        RestoraniPreferences.addRestoran(model, MainActivity.this);
+                        adapter.setItems(generateList());
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+        adapter.setItems(generateList());
+        moeRView.setHasFixedSize(true);
+        moeRView.setLayoutManager(new LinearLayoutManager(this));
+        moeRView.setAdapter(adapter);
+
+
 
     }
+
+    ArrayList<Restorani> generateList() {model = RestoraniPreferences.getRestoran(this);
+        return model.restaurants;}
+
+
 }
